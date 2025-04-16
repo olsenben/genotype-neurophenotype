d3aa282fefa011b77d2708076b9f8b288adcde85
benol <ben.olsen.ben@gmail.com>
2025-04-16 15:47:39 -0400

    eeg preprocessing script finished

--- scripts/preprocess_eeg_data.py
+++ scripts/preprocess_eeg_data.py
@@ -4,16 +4,20 @@ from mne.preprocessing import find_bad_channels_maxwell, ICA
 from mne_icalabel import label_components
 import pandas as pd
 import os
+import traceback
 
 """
 script for preprocessing resting eeg data. 
 if you only want to process eyes open vs eyes closed,
 make sure you comment out the appropriate lines after 
 the event splitting step (Step 8)
+Warning: each ICA fitting can 5-8 minutes. Preprocessing this data will take awhile, go get a coffee or something.
 """
 
 #folder where data is stored
 base_path = "E:/neuro_data/ds004796"
+
+error_log = {}
 
 def get_segments(annotations):
     #decscriptions are "event_type"
@@ -62,90 +66,106 @@ def get_segments(annotations):
 
     return (open_start, open_end), (closed_start, closed_end)
 
-#make data directory
-os.makedirs(f"{base_path}/processed_data", exist_ok=True)
-
 for i in range(1, 81):
-    
-    #STEP 1: Load Data
     subject = f"sub-{i:02d}"
-    print(f"Processing folder: {subject}")
-    file_prefix = (base_path, subject, "eeg")
-    vhdr_file = os.path.join(file_prefix, f"{subject}_task-rest_eeg.vhdr")
-    tsv_file = os.path.join(file_prefix, f"{subject}_task-rest_events.tsv")
-
-    raw = mne.io.read_raw_brainvision(vhdr_file, preload=True)
-    
-    events_df = pd.read_csv(tsv_file, sep='\t')
-
-    #STEP 2: Filter 1-100 Hz
-    raw.filter(1.,100., fir_design='firwin')
-
-    #STEP 3: Downsample
-    raw_downsampled = raw.copy().resample(sfreq=250)
-
-    #STEP 4: Set CAR
-    raw_downsampled.set_eeg_reference('average')
-
-    #STEP 5: Rename Channels and set Montage
-    raw_downsampled.rename_channels(lambda name: name.strip('.'))
-
-    #SETP 6: Redimensioning 
-
-    #STEP 7: Marking Bad Channels
     try:
-        raw_downsampled.info['bads'],_ = find_bad_channels_maxwell(raw)
-    except ValueError as e:
-        print(e)
+        #STEP 1: Load Data
+        print(f"Processing folder: {subject}")
+        file_prefix = os.path.join(base_path, subject, "eeg")
+        vhdr_file = os.path.join(file_prefix, f"{subject}_task-rest_eeg.vhdr")
+        tsv_file = os.path.join(file_prefix, f"{subject}_task-rest_events.tsv")
 
-    #STEP 8: Set annotations and split Events
-    annotations = mne.Annotations(
-    onset=events_df['onset'].values,
-    duration=events_df['duration'].values,
-    description=events_df['event_type'].values
-    )
+        raw = mne.io.read_raw_brainvision(vhdr_file, preload=True)
+        
+        events_df = pd.read_csv(tsv_file, sep='\t')
 
-    raw_downsampled.set_annotations(annotations)
-    (open_start, open_end), (closed_start, closed_end) = get_segments(raw_downsampled.annotations)
+        #STEP 2: Filter 1-100 Hz
+        raw.filter(1.,100., fir_design='firwin')
 
-    #raw_downsampled_open = raw.copy().crop(tmin=open_start, tmax=open_end)
-    raw_downsampled_closed = raw.copy().crop(tmin=closed_start, tmax=closed_end)
+        #STEP 3: Downsample
+        raw_downsampled = raw.copy().resample(sfreq=250)
+
+        #STEP 4: Set CAR
+        raw_downsampled.set_eeg_reference('average')
+
+        #STEP 5: Rename Channels and set Montage
+        raw_downsampled.rename_channels(lambda name: name.strip('.'))
+
+        #SETP 6: Redimensioning 
+
+        #STEP 7: Marking Bad Channels
+        try:
+            raw_downsampled.info['bads'],_ = find_bad_channels_maxwell(raw)
+        except ValueError as e:
+            print(e)
+
+        #STEP 8: Set annotations and split Events
+        annotations = mne.Annotations(
+        onset=events_df['onset'].values,
+        duration=events_df['duration'].values,
+        description=events_df['event_type'].values
+        )
+
+        raw_downsampled.set_annotations(annotations)
+        (open_start, open_end), (closed_start, closed_end) = get_segments(raw_downsampled.annotations)
+
+        #raw_downsampled_open = raw.copy().crop(tmin=open_start, tmax=open_end)
+        raw_downsampled_closed = raw.copy().crop(tmin=closed_start, tmax=closed_end)
+        
+        #STEP 9: Artifact Removal
+        ica = ICA(n_components=.99,method='picard',fit_params=dict(ortho=False, extended=True), random_state=97)
+
+        #ica.fit(raw_downsampled_open)
+        ica.fit(raw_downsampled_closed)
+
+        labels = label_components(raw_downsampled_closed, ica, method='iclabel')
+
+        bad_labels = ['eye','heart','muscle']
+
+        bad_idx = [i for i, label in enumerate(labels['labels']) if label in bad_labels]
+
+        print(f"Removing ICA components: {bad_idx} - {', '.join(labels['labels'][i] for i in bad_idx)}")
+
+        ica.exclude = bad_idx
+
+        #raw_clean_open = ica.apply(raw_downsampled_open)
+        raw_clean_closed = ica.apply(raw_downsampled_closed)
+
+        #raw_clean_open.interpolate_bads()
+        raw_clean_closed.interpolate_bads()
+
+        #STEP 10: Filter 0.5-60 Hz
+        iir_params = dict(order=5, ftype='butter', output='sos')
+        
+        raw_clean_closed.filter(l_freq=0.5, h_freq=None, method='iir', iir_params=iir_params)
+
+        raw_clean_closed.filter(l_freq=None, h_freq=60, method='fir', fir_design='firwin')
+
+        epochs_closed = make_fixed_length_epochs(raw_clean_closed, duration=10, preload=True)
+
+        #StEP 11: Save to disk
+        save_dir = os.path.join("E:/neuro_data/processed/derivatives",subject, "eeg")
+        os.makedirs(save_dir, exist_ok=True)
+
+        save_path = os.path.join(save_dir, f"{subject}_task-rest_preproc-epo.fif")
+        
+        epochs_closed.save(save_path, overwrite=True)
     
-    #STEP 9: Artifact Removal
-    ica = ICA(n_components=.99,method='picard',fit_params=dict(ortho=False, extended=True), random_state=97)
+    except Exception as e:
+        print(f"Error Processing {subject}: {str(e)}")
+        error_log[subject] = traceback.format_exc()
 
-    #ica.fit(raw_downsampled_open)
-    ica.fit(raw_downsampled_closed)
+#save error txt
+if error_log:
+    with open("preprocessing_errors.txt", "w") as f:
+        for subject, error_trace in error_log.items():
+            f.write(f"-----{subject}-----\n")
+            f.write(error_trace + "\n\n")
+    print("Some files have failed. See 'preprocessing_errors' for details")
 
-    labels = label_components(raw_downsampled_closed, ica, method='iclabel')
+else:
+    print("All files process successfully")
 
-    bad_labels = ['eye','heart','muscle']
-
-    bad_idx = [i for i, label in enumerate(labels['labels']) if label in bad_labels]
-
-    print(f"Removing ICA components: {bad_idx} - {', '.join(labels['labels'][i] for i in bad_idx)}")
-
-    ica.exclude = bad_idx
-
-    #raw_clean_open = ica.apply(raw_downsampled_open)
-    raw_clean_closed = ica.apply(raw_downsampled_closed)
-
-    #raw_clean_open.interpolate_bads()
-    raw_clean_closed.interpolate_bads()
-
-    #STEP 10: Filter 0.5-60 Hz
-    iir_params = dict(order=5, ftype='butter', output='sos')
-    
-    raw_clean_closed.filter(l_freq=0.5, h_freq=None, method='iir', iir_params=iir_params)
-
-    raw_clean_closed.filter(l_freq=None, h_freq=60, method='fir', fir_design='firwin')
-
-    epochs_closed = make_fixed_length_epochs(raw_clean_closed, duration=10, preload=True)
-
-    #StEP 11: Save to disk
-    fif_file = os.path.join(base_path,"processed_data", {subject}, "eeg", f"{subject}_processed_task-rest_eeg.fif")
-
-    epochs_closed.save(fif_file, overwrite=True)
     
 
 
